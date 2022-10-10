efe677f2d9f5fcdf07847530e1028f2e1722da37
bajrangjangid <bajrang07lal@gmail.com>
2022-10-10 09:19:07 +0530

    initial commit

---
+++ com/src/MinCostClimbingStairs/MinCostClimbingStairs.java
@@ -0,0 +1,13 @@
+package MinCostClimbingStairs;
+// 746. Min Cost Climbing Stairs
+public class MinCostClimbingStairs {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+
+        return 0;
+    }
+}
