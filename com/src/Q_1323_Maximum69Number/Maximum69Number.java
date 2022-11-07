66a45ab1e049c314d2996b78809a304d99f018ad
bajrangjangid <bajrang07lal@gmail.com>
2022-11-07 12:21:17 +0530

    initial commit

--- com/src/Q_1323_Maximum69Number/Maximum69Number.java
+++ com/src/Q_1323_Maximum69Number/Maximum69Number.java
@@ -4,10 +4,10 @@ public class Maximum69Number {
 
     public static void main(String[] args) {
         int num = 9669;
-        System.out.println(maximum69Number(num));
+        System.out.println(approach1(num));
     }
 
-    public static int maximum69Number (int num) {
+    public static int approach1 (int num) {
         StringBuilder sb = new StringBuilder();
         sb.append(num);
 
@@ -18,5 +18,36 @@ public class Maximum69Number {
             }
         }
         return Integer.parseInt(sb.toString());
+    }
+
+    public int approach2 (int num) {
+        // Convert the input 'num' to the string 'numString'.
+        String numString = "" + num;
+
+        // Use the built-in function to replace the first '6' with '9'.
+        // Return the integer converted from the modified 'numString'.
+        return Integer.parseInt(numString.replaceFirst("6", "9"));
+    }
+
+    public int approach3 (int num) {
+        // Since we start with the lowest digit, initialize currDigit = 0.
+        int numCopy = num;
+        int indexSix = -1;
+        int currDigit = 0;
+
+        // Check every digit of 'numCopy' from low to high.
+        while (numCopy > 0) {
+            // If the current digit is '6', record it as the highest digit of 6.
+            if (numCopy % 10 == 6)
+                indexSix = currDigit;
+
+            // Move on to the next digit.
+            numCopy /= 10;
+            currDigit++;
+        }
+
+        // If we don't find any digit of '6', return the original number,
+        // otherwise, increment 'num' by the difference made by the first '6'.
+        return indexSix == -1 ? num : num + 3 * (int)Math.pow(10, indexSix);
     }
 }
